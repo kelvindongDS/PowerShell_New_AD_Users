f797b82b1c89c664e7c50d955bc12d8f8d385a24
kelvindongDS <40708057+kelvindongDS@users.noreply.github.com>
2018-09-23 16:14:32 +1200

    Using Powershell for Windows Server 2016 for AD accounts

---
+++ Create account by CSV.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module ActiveDirectory
+
+#read from CSV file
+$filepath = Read-Host -Prompt "Enter CSV path:"
+$users = Import-Csv $filepath
+
+#default password 
+$securePassword= ConvertTo-SecureString "TestingPass123" -AsPlainText -Force
+
+#Lop for each user in user list
+foreach ($user in $users) 
+{
+    $fname= $user.'First Name'
+    $lname = $user.'Last Name'
+    $email = $user.'Email Address'
+    $jtitle = $user.'Job Title'
+    $officeCell= $user.'Office Phone'
+    $description = $user.Description
+    $OUpath= $user.'Organizational Unit'
+
+    echo "Account created for $fname $lname in $OUpath"
+
+    #create new AD User
+    New-ADUser -Name "$fname $lname" -GivenName $fname -Surname $lname -UserPrincipalName "$fname.$lname" -Path $OUpath -AccountPassword $securePassword -ChangePasswordAtLogon $true -OfficePhone $officeCell -Description $description -Enabled $true -EmailAddress $email 
+}
---
+++ create new account.ps1
@@ -0,0 +1,23 @@
+﻿
+Import-Module ActiveDirectory
+$exit= ""
+While ($exit -ne "q")
+{   
+    #input data
+    $firstname= Read-Host -Prompt "input ur fname"
+    $lastname = Read-Host -Prompt "input ur lastname"
+    #create standard pass, users are forced to change when logging in
+    $password = "Admin1234"
+
+    echo "ur full name is $firstname $lastname . Yr password is $password"
+    
+    #define location 
+    $OUpath = "OU=PowerShellUsers,DC=itflee,DC=com"
+    
+    #hash password
+    $securePassord = ConvertTo-SecureString $password -AsPlainText -Force
+    
+    #create new AD user
+    New-ADUser -Name "$firstname $lastname" -GivenName "$firstname" -Surname "$lastname" -UserPrincipalName "$firstname.$lastname" -Path $OUpath -AccountPassword $securePassord -ChangePasswordAtLogon $true -Enabled $true
+    $exit = Read-Host -Prompt "type q to exit"
+}
